8d3268b8ae5f75273f30db182ff7382a059f5dcf
Piyush <piyushtiwarindls220@gmail.com>
2024-09-22 21:12:14 +0530

    completed latex rendering

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -4,6 +4,7 @@ import { Group } from "@mantine/core";
 import { Button } from "@/components/ui/button";
 import Draggable from "react-draggable";
 import axios from "axios";
+import { Link } from "react-router-dom";
 
 interface Response {
   expr: string;
@@ -38,19 +39,17 @@ export default function HomePage() {
 
   useEffect(() => {
     if (latexExpression.length > 0 && window.MathJax) {
-      setTimeout(() => {
-        if (typeof window.MathJax !== "undefined") {
-          window.MathJax.Hub.Queue(["Typeset", window.MathJax.Hub]);
-        }
-      }, 0);
+      window.MathJax.Hub.Queue(["Typeset", window.MathJax.Hub]);
     }
   }, [latexExpression]);
+
   const renderLatexToCanvas = useCallback(
     (expression: string, answer: string) => {
       const latex = `\\(\\LARGE{${expression} = ${answer}}\\)`;
-      setLatexExpression((prevLatex) => [...prevLatex, latex]); // Use functional update for state
 
-      // Clear the main canvas
+      // const latex = `\\(${answer}\\)`;
+      setLatexExpression((prevLatex) => [...prevLatex, latex]);
+
       const canvas = canvasRef.current;
       if (canvas) {
         const ctx = canvas.getContext("2d");
@@ -59,7 +58,7 @@ export default function HomePage() {
         }
       }
     },
-    [canvasRef] // List of dependencies
+    [canvasRef]
   );
 
   useEffect(() => {
@@ -80,23 +79,32 @@ export default function HomePage() {
         context.lineWidth = 4;
       }
     }
+  }, []);
 
-    // const script = document.createElement("script");
-    // script.src =
-    //   "https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.9/config/TeX-MML-AM_CHTML.js";
-    // script.async = true;
-    // document.head.appendChild(script);
+  useEffect(() => {
+    const loadMathJax = () => {
+      const script = document.createElement("script");
+      script.type = "text/javascript";
+      script.src =
+        "https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.9/latest.js?config=TeX-MML-AM_CHTML";
+      script.async = true;
+      script.onload = () => {
+        if (window.MathJax) {
+          window.MathJax.Hub.Config({
+            tex2jax: {
+              inlineMath: [
+                ["$", "$"],
+                ["\\(", "\\)"],
+              ],
+            },
+          });
+          window.MathJax.Hub.Queue(["Typeset", window.MathJax.Hub]);
+        }
+      };
+      document.head.appendChild(script);
+    };
 
-    if (window.MathJax) {
-      window.MathJax.Hub.Config({
-        tex2jax: {
-          inlineMath: [
-            ["$", "$"],
-            ["\\(", "\\)"],
-          ],
-        },
-      });
-    }
+    loadMathJax();
   }, []);
 
   const resetCanvas = () => {
@@ -155,7 +163,6 @@ export default function HomePage() {
       const resp = await res.data;
       console.log("Response", resp);
       if (resp.message === "Image processed") {
-        // Parse the string data to convert it into an array of objects
         const parsedData = JSON.parse(resp.data.replace(/'/g, '"'));
         console.log(parsedData);
         parsedData.forEach((data: Response) => {
@@ -195,20 +202,17 @@ export default function HomePage() {
 
       setLatexPosition({ x: centerX, y: centerY });
       if (resp.message === "Image processed") {
-        // Parse the string data to convert it into an array of objects
         const parsedData = JSON.parse(resp.data.replace(/'/g, '"'));
         console.log(parsedData);
         parsedData.forEach((data: Response) => {
-          // if (data.assign === true) {
           setTimeout(() => {
             setResult((prevResult) => ({
-              ...prevResult, // Maintain any previous result properties
+              ...prevResult,
               expression: data.expr,
               answer: data.result,
             }));
           }, 1000);
           console.log(result);
-          // }
         });
       }
     }
@@ -216,34 +220,79 @@ export default function HomePage() {
 
   return (
     <>
-      <div className="grid grid-cols-[400px_auto_400px] gap-4 items-center">
+      <div className="flex flex-wrap justify-between items-center p-2 bg-black ">
         <Button
           onClick={() => setReset(true)}
-          className="z-20 bg-black text-white font-semibold text-lg"
+          className="z-20 h-10 px-4 py-2 bg-red-500 text-white m-1 font-semibold"
           variant="default"
-          color="black"
+          color="red"
         >
           Reset
         </Button>
-        <Group className="z-20 space-x-1 flex justify-center items-center gap-2">
+        <Group className="z-20 flex flex-wrap justify-center">
           {SWATCHES.map((Color: string) => (
             <button
               key={Color}
               style={{
                 backgroundColor: Color,
-                width: "40px",
-                height: "40px",
+                width: "30px",
+                height: "30px",
                 borderRadius: "50%",
+                margin: "2px",
+                cursor: "pointer",
+                borderStyle: "none",
+                position: "relative",
               }}
               onClick={() => setColor(Color)}
             />
           ))}
         </Group>
         <Button
-          onClick={sendData}
-          className="z-20 bg-black text-white font-semibold text-lg"
+          className="h-10 px-4 py-2 z-20 bg-gray-500 text-white m-1"
           variant="default"
-          color="black"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            className="lucide lucide-eraser"
+          >
+            <path d="m7 21-4.3-4.3c-1-1-1-2.5 0-3.4l9.6-9.6c1-1 2.5-1 3.4 0l5.6 5.6c1 1 1 2.5 0 3.4L13 21" />
+            <path d="M22 21H7" />
+            <path d="m5 11 9 9" />
+          </svg>
+        </Button>
+        <div className="flex items-center space-x-2 bg-gray-700 p-2 rounded z-20 text-white">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="20"
+            height="20"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            className="lucide lucide-pen"
+          >
+            <path d="M21.174 6.812a1 1 0 0 0-3.986-3.987L3.842 16.174a2 2 0 0 0-.5.83l-1.321 4.352a.5.5 0 0 0 .623.622l4.353-1.32a2 2 0 0 0 .83-.497z" />
+          </svg>
+          <div>
+            <input className="cursor-pointer" type="range" />
+          </div>
+          <span>3px</span>
+        </div>
+        <Button
+          onClick={sendData}
+          className="h-10 px-4 py-2 z-20 bg-green-500 text-white m-1 font-semibold"
+          variant="default"
+          color="green"
         >
           Calculate
         </Button>
@@ -251,7 +300,7 @@ export default function HomePage() {
       <canvas
         ref={canvasRef}
         id="canvas"
-        className="absolute top-0 left-0 w-full h-full"
+        className="absolute top-0 left-0 w-full h-full bg-black"
         onMouseDown={startDrawing}
         onMouseOut={stopDrawing}
         onMouseUp={stopDrawing}
@@ -269,6 +318,11 @@ export default function HomePage() {
             </div>
           </Draggable>
         ))}
+      <Link to="https://www.github.com/webbedpiyush/litCalc-fe/" target="_blank">
+        <Button className="absolute bottom-4 right-4 z-50 bg-blue-500 text-white font-semibold h-10 px-4 py-4">
+          Github
+        </Button>
+      </Link>
     </>
   );
 }
