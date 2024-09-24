28c43bbef310709b70c580dc4eb885a627130851
Piyush <piyushtiwarindls220@gmail.com>
2024-09-24 11:14:15 +0530

    fixed url endpoint

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import "@/index.css";
 import { MantineProvider } from "@mantine/core";
+import { useEffect } from "react";
+import axios from "axios";
 
 const paths = [
   {
@@ -12,6 +14,17 @@ const paths = [
 
 const browserRouter = createBrowserRouter(paths);
 export default function App() {
+  useEffect(function () {
+    async function startTheServer() {
+      await axios({
+        method: "get",
+        url: `${import.meta.env.VITE_API_URL}`,
+      });
+    }
+
+    startTheServer();
+  }, []);
+
   return (
     <MantineProvider>
       <RouterProvider router={browserRouter}></RouterProvider>
